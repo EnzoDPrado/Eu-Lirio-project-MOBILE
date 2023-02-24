de5c74c2bc7898e9d70b828a7e24552f81b064b2
MarinaSantello <marina.12soft@gmail.com>
2023-02-23 21:36:04 -0300

    atualizando udancas do desenvolvimento da ultima

--- app/src/main/java/com/example/loginpage/RegisterPageThirdPart01.kt
+++ app/src/main/java/com/example/loginpage/RegisterPageThirdPart01.kt
@@ -62,7 +62,6 @@ fun registerPageThirdPart() {
     val context = LocalContext.current
 
     val focusManager = LocalFocusManager.current
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -73,13 +72,13 @@ fun registerPageThirdPart() {
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-//            Image(
-//                painter = painterResource(id = R.drawable.logoeulirio),
-//                contentDescription = "",
-//                modifier = Modifier
-//                    .height(100.dp)
-//                    .width(178.dp)
-//            )
+            Image(
+                painter = painterResource(id = R.drawable.logoeulirio),
+                contentDescription = "",
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(178.dp)
+            )
 
             Column(
                 modifier = Modifier.fillMaxSize(),
@@ -90,42 +89,110 @@ fun registerPageThirdPart() {
                 Card(
                     modifier = Modifier
                         .padding(start = 24.dp, end = 24.dp)
-                        .height(400.dp),
+                        .height(440.dp),
                     shape = RoundedCornerShape(50.dp),
                     elevation = 4.dp,
                     backgroundColor = Color.White
                 ) {
-                    Column(
-                        modifier = Modifier.padding(bottom = 40.dp)
-                    ) {
-                        Text(
-                            text = "olá, [nome]",
-                            modifier = Modifier
-                                .padding(top = 40.dp)
-                                .fillMaxWidth(),
-//                            color = colorResource(id = R.color.eulirio_purple_text_color),
-                            color = Color.Red,
-                            textAlign = TextAlign.Center,
-                            style = MaterialTheme.typography.h2
-                        )
-
-                        Spacer(modifier = Modifier.height(12.dp))
-
+                    Box(){
                         Column(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .verticalScroll(rememberScrollState()),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
+                                .padding(bottom = 4.dp)
                         ) {
-                            Text(text = "teste")
+                            Column(
+                            ) {
+                                Text(
+                                    text = "olá, [nome]".uppercase(),
+                                    modifier = Modifier
+                                        .padding(top = 40.dp)
+                                        .fillMaxWidth(),
+                                    color = colorResource(id = R.color.eulirio_purple_text_color),
+                                    textAlign = TextAlign.Center,
+                                    style = MaterialTheme.typography.h2
+                                )
+
+                                val descriptionP1 = stringResource(id = R.string.description_third_page_pt1)
+                                val descriptionP2 = stringResource(id = R.string.description_third_page_pt2)
+                                val descriptionP3 = stringResource(id = R.string.description_third_page_pt3)
+                                val descriptionP4 = stringResource(id = R.string.description_third_page_pt4)
+                                val descriptionP5 = stringResource(id = R.string.description_third_page_pt5)
+
+                                Text(
+                                    buildAnnotatedString {
+                                        append("$descriptionP1 ")
+
+                                        withStyle(style = SpanStyle(
+                                            color = colorResource(id = R.color.eulirio_purple_text_color),
+                                            fontWeight = FontWeight.W900
+                                        )) {
+                                            append("$descriptionP2 ")
+                                        }
+
+                                        append("$descriptionP3 ")
+
+                                        withStyle(style = SpanStyle(
+                                            color = colorResource(id = R.color.eulirio_purple_text_color),
+                                            fontWeight = FontWeight.W900
+                                        )) {
+                                            append("$descriptionP4 ")
+                                        }
+
+                                        append(descriptionP5)
+                                    },
+
+                                    modifier = Modifier.padding(start = 48.dp, end = 48.dp),
+                                    textAlign = TextAlign.Center,
+                                    style = MaterialTheme.typography.body2,
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.height(12.dp))
+
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .verticalScroll(rememberScrollState()),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+
+                                LazyColumn(
+
+                                ) {
+
+                                }
+                            }
+                        }
+
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                                .background(Color(0x80FFFFFF))
+                                .fillMaxWidth()
+                                .height(70.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Button(onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .width(200.dp),
+                                shape = RoundedCornerShape(30.dp),
+                                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.eulirio_purple))
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.finish),
+                                    color = colorResource(
+                                        id = R.color.white
+                                    ),
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+
+                            }
                         }
                     }
                 }
             }
-
         }
-
     }
 
 }
